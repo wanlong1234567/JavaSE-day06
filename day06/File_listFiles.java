e7f7e8887e8db24e2dd31b6a41c556241911e366
wanlong1234567 <2531165965@qq.com>
2018-04-08 18:06:53 +0800

    遍历文件

---
+++ day06/File_listFiles.java
@@ -0,0 +1,51 @@
+package day06;
+
+import java.io.File;
+
+/**
+ * 使用File获取一个目录中的所有子项
+ * @author adminitartor
+ *
+ */
+public class File_listFiles {
+	public static void main(String[] args) {
+		/*
+		 * 获取当前目录中的所有子项
+		 */
+		File dir = new File(".");
+		/*
+		 * 判断File表示的是文件还是目录
+		 * boolean isFile()
+		 * boolean isDirectory()
+		 */
+		if(dir.isDirectory()){
+			/*
+			 * File[] listFiles()
+			 * 获取当前File表示的目录中的所有子项
+			 * 返回的数组中每个元素为该目录中的一个
+			 * 子项。
+			 */
+			File[] subs = dir.listFiles();
+			for(File sub : subs){
+				if(sub.isFile()){
+					System.out.print("文件:");
+				}
+				if(sub.isDirectory()){
+					System.out.print("目录:");
+				}
+				System.out.println(sub.getName());
+			}
+		}
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
